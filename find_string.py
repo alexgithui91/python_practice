fb237c77bff8c6ed9bdf870ac9b3204c79f50fec
Alex Githui <agithui@dayliff.com>
2022-01-05 12:29:55 +0300

    working find_string

---
+++ find_string.py
@@ -0,0 +1,20 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if (i + len(sub_string) - 1) > len(string) - 1:
+            pass
+        else:
+            if string[i : ((i + len(sub_string) - 1) + 1)] == sub_string:
+                count += 1
+            else:
+                pass
+
+    return count
+
+
+if __name__ == "__main__":
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
