1871c97092277ecae96ff7bf1a405edf357f97cc
alexgithui91 <alexgithui91@gmail.com>
2022-01-11 12:52:42 +0300

    working reverse_capitalize

--- reverse_capitalize.py
+++ reverse_capitalize.py
@@ -7,16 +7,34 @@ import sys
 
 def reverse_words_order_and_swap_cases(sentence):
     # Write your code here
-    pass
+    new_sentence = sentence.split()
+    new_sentence.reverse()
+
+    new_string = ""
+    new_list = []
+
+    for elem in new_sentence:
+        for letter in elem:
+            if letter.isupper():
+                new_string += letter.lower()
+            else:
+                new_string += letter.upper()
+
+        new_list.append(new_string)
+        new_string = ""
+
+    return " ".join(new_list)
 
 
 if __name__ == "__main__":
-    fptr = open(os.environ["OUTPUT_PATH"], "w")
+    # fptr = open(os.environ["OUTPUT_PATH"], "w")
 
-    sentence = input()
+    # sentence = input()
+    sentence = "fUn"
 
     result = reverse_words_order_and_swap_cases(sentence)
 
-    fptr.write(result + "\n")
+    # fptr.write(result + "\n")
 
-    fptr.close()
+    # fptr.close()
+    print(result)
