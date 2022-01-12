a28e91deaea740c679cd1a3d68748f4ae52fce70
alexgithui91 <alexgithui91@gmail.com>
2022-01-12 10:33:33 +0300

    working code for calendar

--- working_with_calendar.py
+++ working_with_calendar.py
@@ -1,3 +1,12 @@
 import calendar
 
-print(calendar.TextCalendar(firstweekday=7).formatyear(2022))
+# Interesting way to print out entire calendar
+# print(calendar.TextCalendar(firstweekday=7).formatyear(2022))
+
+date_list = list(map(int, input().split()))
+
+day_num = calendar.weekday(date_list[2], date_list[0], date_list[1])
+
+day_name = calendar.day_name[day_num]
+
+print(day_name.upper())
